a6857c98470d42e47d691b84d18d47b895195b36
Juan Rafael Valerio M <juanra_vm@hotmail.com>
2018-09-03 14:43:45 -0600

    Add new gets
    Add new gets

--- server/controllers/rules.controller.js
+++ server/controllers/rules.controller.js
@@ -1,6 +1,7 @@
 //var data = require('../../Data.json');
 const editJsonFile = require("edit-json-file");
 let file = editJsonFile(`${__dirname}/../../Data.json`);
+let file2 = editJsonFile(`${__dirname}/../../Config.json`);
 var methods = require('../../modules/reversie')
 const rulesCtrl = {}
 
@@ -25,9 +26,35 @@ rulesCtrl.tryMove = (req,res)=>{
     }
     res.json(object.matrix);
 }
+
+rulesCtrl.configBoard = async (req,res)=>{
+    await file2.set("boardStyle",req.body.boardStyle)
+    await file2.set("pieceStyle",req.body.pieceStyle)
+    await file2.save()
+    res.json(req.body.pieceStyle);
+}
+
 rulesCtrl.getPlayers = async (req,res)=>{
     let players = await (file.get("players"))
-    res.json(players);
+    if(players!==undefined)
+        res.json(players);
+    else
+        res.json("")
+}
+rulesCtrl.getBoardStyle = async (req,res)=>{
+    let players = await (file2.get("boardStyle"))
+    if(players!==undefined)
+        res.json(players);
+    else
+        res.json("")
+}
+rulesCtrl.getPieceStyle = async (req,res)=>{
+    let players = await (file2.get("pieceStyle"))
+    //console.log(players)
+    if(players!==undefined)
+        res.json(players);
+    else
+        res.json("")
 }
 rulesCtrl.createMatrix= async (req,res)=>{
     let matrix = await methods.crearTablero(req.body.size);
--- server/routes/rules.routes.js
+++ server/routes/rules.routes.js
@@ -4,7 +4,10 @@ const rulesCtrl = require('../controllers/rules.controller')
 
 router.get('/getMatrix',rulesCtrl.getMatrix)//pide
 router.get('/getPlayers',rulesCtrl.getPlayers)//pide
+router.get('/getBoardStyle',rulesCtrl.getBoardStyle)//pide
+router.get('/getPieceStyle',rulesCtrl.getPieceStyle)//pide
 router.put('/rules/:posX/:posY',rulesCtrl.tryMove)//actualiza
+router.put('/config/board',rulesCtrl.configBoard)//actualiza
 router.post('/createMatrix',rulesCtrl.createMatrix)//crea matriz
 //router.post//crea datos
 //router.delete//elimina datos
